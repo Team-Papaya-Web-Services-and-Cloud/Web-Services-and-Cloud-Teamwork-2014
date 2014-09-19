16fa77c15a72379af8e41b369dcc7bf9ec1fbe4e
Martin Nikolov <flextry@gmail.com>
2014-09-19 11:02:39 +0300

    VotesController created

---
+++ Votter/Votter.Services/Controllers/VotesController.cs
@@ -0,0 +1,22 @@
+﻿namespace Votter.Services.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Web.Http;
+    using Votter.Models;
+
+    [Authorize]
+    public class VotesController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Create(int upVotePictureId, int downVotePictureId)
+        {
+            Vote vote = null;
+
+            // TODO Validation
+            // Authorized
+
+            return Ok(vote);
+        }
+    }
+}
