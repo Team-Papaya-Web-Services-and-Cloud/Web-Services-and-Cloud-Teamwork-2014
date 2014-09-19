6c9dea44abcbbd78c1a1bd56e29a30b12b356a66
IvailoPapazov <ihellfiend@gmail.com>
2014-09-19 12:58:40 +0300

    Added method create to votes controller

--- Votter/Votter.Services/Controllers/VotesController.cs
+++ Votter/Votter.Services/Controllers/VotesController.cs
@@ -4,19 +4,64 @@
     using System.Linq;
     using System.Web.Http;
     using Votter.Models;
+    using Votter.Data;
+using Votter.Services.Models;
 
-    [Authorize]
+    //[Authorize]
     public class VotesController : ApiController
     {
-        [HttpPost]
-        public IHttpActionResult Create(int upVotePictureId, int downVotePictureId)
+        private VotterData data;
+
+        public VotesController()
         {
-            Vote vote = null;
+            data = new VotterData();
+        }
 
-            // TODO Validation
-            // Authorized
+        [HttpPost]
+        public IHttpActionResult Create([FromBody]VoteModel vote)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Created vote is not valid!");
+            }
 
-            return Ok(vote);
+            // Check if pictures exists
+            var upVotePicture = this.data.Pictures.Find(vote.UpVotePictureId);
+            var downVotePicture = this.data.Pictures.Find(vote.DownVotePictureId);
+            if (upVotePicture == null || downVotePicture == null)
+            {
+                return BadRequest("Cannot find pictures in database!");
+            }
+
+            // Check if ids are equal
+            if (vote.UpVotePictureId == vote.DownVotePictureId)
+            {
+                return BadRequest("Cannot compare pictures that are the same!");
+            }
+
+            // Check if ids belongs to a same person
+            if (upVotePicture.ApplicationUserId == downVotePicture.ApplicationUserId)
+            {
+                return BadRequest("Cannot compare pictures that belongs to a same person!");
+            }
+
+            // Check if pictures are from different categories
+            if (upVotePicture.CategoryId != downVotePicture.CategoryId)
+            {
+                return BadRequest("Cannot compare pictures from different categories!");
+            }
+
+            var newVote = new Vote
+            {
+                ApplicationUserId = vote.UserId,
+                DownVotePicture = downVotePicture,
+                UpVotePicture = upVotePicture,
+            };
+
+            this.data.Votes.Add(newVote);
+            this.data.SaveChanges();
+
+            return Ok(newVote.VoteId);
         }
     }
 }
